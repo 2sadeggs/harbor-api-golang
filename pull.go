6270278cd57e7a17f2c0ed3d5ef22d602393d9fc
root <root@localhost.localdomain>
2024-05-28 10:53:53 +0800

    bugfix

--- pull.go
+++ pull.go
@@ -67,7 +67,7 @@ func downloadAndSaveArtifacts(baseURL, auth string) error {
 	}
 
 	// 创建一个清单文件
-	listFilePath := filepath.Join(savePath, "all_uri_list.txt")
+	listFilePath := filepath.Join(savePath, "download_list.txt")
 	listFile, err := os.Create(listFilePath)
 	if err != nil {
 		return fmt.Errorf("failed to create list file: %v", err)
